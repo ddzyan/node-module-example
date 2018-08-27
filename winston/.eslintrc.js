707fdfd37ce72e854d29cd92d83a4b9833fab170
dingdz <ddzyan@163.com>
2018-08-27 14:03:24 +0800

    初始化winston项目

---
+++ winston/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "env": {
+        "node": true
+    },
+    "rules": {
+        "linebreak-style": [0, "error", "windows"],
+        "no-console": "off",
+        "quotes": ["error", "single"],
+        "no-plusplus": "off"
+    }
+};
