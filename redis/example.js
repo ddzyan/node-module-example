c70bd4c9239f7cf8fc7f435ce248582c474aa486
dingdz <ddzyan@163.com>
2018-12-18 02:10:12 +0800

    增加redis功能

--- redis/example.js
+++ redis/example.js
@@ -1,20 +1,64 @@
 const redis = require("redis"),
     client = redis.createClient(6379, '127.0.0.1');
 
-client.select(0, function () {
-    console.log("connnet sucess");
-    client.on("error", function (err) {
-        console.log("Error " + err);
-    });
-
-    client.set("string key", "string val", redis.print);
-    client.hset("hash key", "hashtest 1", "some value", redis.print);
-    client.hset(["hash key", "hashtest 2", "some other value"], redis.print);
-    client.hkeys("hash key", function (err, replies) {
-        console.log(replies.length + " replies:");
-        replies.forEach(function (reply, i) {
-            console.log("    " + i + ": " + reply);
-        });
-        client.quit();
-    });
+client.on("connect", function () {
+    console.log("connect success");
 });
+
+client.on("error", function (err) {
+    console.log("Error");
+});
+
+client.on("end", function (err) {
+    console.log("end : " + err);
+});
+
+
+async function redisInit() {
+    try {
+        client.select(0);
+
+        await redisString();
+
+        await redisHash();
+
+        client.quit();
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function redisString() {
+    return new Promise((resolve, reject) => {
+        client.set("apple", "苹果", redis.print);
+
+        client.get("apple", function (err, reply) {
+            if (err) {
+                reject(err);
+            } else {
+                console.log(reply.toString()); // Will print `OK`
+                resolve();
+            }
+        });
+    })
+
+}
+
+function redisHash() {
+    return new Promise((resolve, reject) => {
+        client.hset("fruitList", "banana", "香蕉", redis.print);
+
+        client.hget("fruitList", "banana", function (err, reply) {
+            if (err) {
+                reject(err);
+            } else {
+                console.log(reply.toString()); // Will print `OK`
+                resolve();
+            }
+        });
+    })
+
+}
+
+redisInit();
