cecee96c6222016f1bb768da77262db5a3703d61
丁东照 <dingdz@sanlogic.com>
2018-12-25 11:27:58 +0800

    添加sequlize配置文件,添加sequelize-auto模块使用方法

--- sequelize/models/index.js
+++ sequelize/models/index.js
@@ -1,33 +1,36 @@
-const Sequelize = require('sequelize');
-const fs = require('fs');
+const Sequelize = require("sequelize");
+const fs = require("fs");
 const path = require("path");
 const basename = path.basename(__filename);
-const sequelize = new Sequelize("demo", "test", "w6ecb0qljc", {
-    host: "111.231.215.55",
-    port: 3306,
-    dialect: "mysql",
-    pool: {
-        max: 10,
-        min: 0,
-        acquire: 30000,
-        idle: 10000 //空闲时间
-    }
-});
+
+const { mysql_db } = require("../config");
+const sequelize = new Sequelize(
+  mysql_db.database,
+  mysql_db.username,
+  mysql_db.password,
+  mysql_db.options,
+  mysql_db.define,
+  mysql_db.operatorsAliases
+);
 
 const db = [];
 
-fs.readdirSync(__dirname).filter(file => {
-    return (file.indexOf(".") !== 0 && (file !== basename) && (file.slice(-3) === '.js'));
-}).forEach(file => {
-    const model = sequelize['import'](path.join(__dirname, file));
+fs.readdirSync(__dirname)
+  .filter(file => {
+    return (
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+    );
+  })
+  .forEach(file => {
+    const model = sequelize["import"](path.join(__dirname, file));
     db[model.name] = model;
-});
+  });
 
 Object.keys(db).forEach(modelName => {
-    if (db[modelName].associate) {
-        db[modelName].associate(db);
-    }
-})
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
 
 /* sequelize.authenticate().then(() => {
     console.log("connect success");
