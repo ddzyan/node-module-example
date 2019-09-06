30160e4767b0d7c7d03a3c4552dea0ce94d2829c
丁东照 <dingdz@sanlogic.com>
2019-09-06 17:15:35 +0800

    调整目录

--- randomDemo/src/random.js
+++
@@ -1,9 +0,0 @@
-const randomstring = require("randomstring");
-
-exports.getRandomString = function(length = 15, charset = "alphabetic") {
-  const randomString = randomstring.generate({
-    length,
-    charset
-  });
-  return randomString;
-};
