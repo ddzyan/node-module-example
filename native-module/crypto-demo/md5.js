febb0c25242c576acbdfa253cf157fb57d7505e1
ddz <ddzyan@163.com>
2020-07-27 14:33:29 +0800

    去除无关内容

--- native-module/crypto-demo/md5.js
+++ native-module/crypto-demo/md5.js
@@ -1,8 +1,8 @@
 const crypto = require('crypto');
-const md5 = require('md5');
+// const md5 = require('md5');
 
 const algorithm = 'md5';
-const MD5Encryption = function(data, key) {
+const MD5Encryption = function (data, key) {
   const encryption = crypto.createHash(algorithm); //创建指定加密算法的对象
   let encryptionted = encryption.update(data).digest('hex');
   return encryptionted;
@@ -15,4 +15,4 @@ const encrypted = MD5Encryption(message);
 
 console.log('%s 加密后：', message, encrypted);
 
-console.log('%s 使用Md5模块加密后：', message, md5(message));
+//console.log('%s 使用Md5模块加密后：', message, md5(message));
